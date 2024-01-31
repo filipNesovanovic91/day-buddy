5c79de08042970b4dad759cba93a8b2d39a5287d
filipnesovanovic <filip.nesovanovic@stacksports.com>
2024-02-01 00:18:16 +0100

    Added prompt input

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainChatComponent } from './components/chat/main-chat.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    MainChatComponent 
   ],
   imports: [
     CommonModule,
