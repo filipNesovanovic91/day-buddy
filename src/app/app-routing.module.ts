f8a1bfb250c285a6a5c1f20708ae5b7b24a5bc3e
Vuk Todoric <vuk.todoric@ey.com>
2024-02-01 15:26:49 +0100

    Created login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,33 +5,51 @@ import { NotFoundComponent } from './modules/shared/components/not-found.compone
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule),
   },
   {
     path: 'work-support',
-    loadChildren: () => import('./modules/work-support/work-support.module').then(m => m.WorkSupportModule) 
+    loadChildren: () =>
+      import('./modules/work-support/work-support.module').then(
+        (m) => m.WorkSupportModule
+      ),
   },
   {
     path: 'emotional-support',
-    loadChildren: () => import('./modules/emotional-support/emotional-support.module').then(m => m.EmotionalSupportModule) 
+    loadChildren: () =>
+      import('./modules/emotional-support/emotional-support.module').then(
+        (m) => m.EmotionalSupportModule
+      ),
   },
   {
     path: 'virtual-coffee-break',
-    loadChildren: () => import('./modules/virtual-coffee-break/virtual-coffee-break.module').then(m => m.VirtualCoffeeBreakModule) 
+    loadChildren: () =>
+      import('./modules/virtual-coffee-break/virtual-coffee-break.module').then(
+        (m) => m.VirtualCoffeeBreakModule
+      ),
   },
   {
     path: 'sport-activity-organizer',
-    loadChildren: () => import('./modules/sport-activity-organizer/sport-activity-organizer.module').then(m => m.SportActivityOrganizerModule) 
+    loadChildren: () =>
+      import(
+        './modules/sport-activity-organizer/sport-activity-organizer.module'
+      ).then((m) => m.SportActivityOrganizerModule),
   },
   {
     path: 'not-found', // Define a path for the not-found component (optional)
-    component: NotFoundComponent // Component to be displayed for not-found
+    component: NotFoundComponent, // Component to be displayed for not-found
   },
-  { path: '**', redirectTo: '/not-found' }
+  { path: '**', redirectTo: '/not-found' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
